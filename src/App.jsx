eb3b70bc1afbfaaf654abb4174b7169e0fceb0ef
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-03-07 22:32:52 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MyContactsPage from 'pages/MyContactsPage/MyContactsPage';
 import Navigation from 'components/Navigation/Navigation';
+import UserRoutes from 'UserRoutes';
 
 import styles from './App.module.css';
 
@@ -10,7 +11,8 @@ function App() {
   return (
   <div className={styles.container}>
     <Navigation/>
-    <MyContactsPage/>
+    <UserRoutes/>
+    {/* <MyContactsPage/> */}
     </div>
   );
 }
