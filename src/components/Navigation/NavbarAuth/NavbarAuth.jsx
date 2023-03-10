4e3fbe3b5dd7abd8281883574bc01a5f11790664
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-03-10 23:10:06 +0200

    icon

--- src/components/Navigation/NavbarAuth/NavbarAuth.jsx
+++ src/components/Navigation/NavbarAuth/NavbarAuth.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import styles from "./navbar-auth.module.css";
 
+
 const getClassName = ({isActive}) => {
     const className = isActive ? `${styles.link} ${styles.active}` : styles.link;
     return className;
@@ -11,7 +12,7 @@ const NavbarAuth =()=> {
     return (
         <div className={styles.wrapper}>
             <NavLink className={getClassName} to="/register">Register</NavLink>
-            <NavLink className={getClassName} to="/login">Login</NavLink>
+            <NavLink className={getClassName} to="/login">Login  </NavLink>
         </div>
     )
 }
