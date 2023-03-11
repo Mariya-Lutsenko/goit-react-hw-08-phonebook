26c8c41d55a036c00e869292439f57a3a8fbffa1
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-03-11 14:25:11 +0200

    animate homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import Phone from '../HomePage/Images/phone.gif';
 import styles from './HomePage.module.css';
+import 'animate.css';
 const HomePage = () => {
   return (
     <main>
       <section className={styles.container}>
-        <h1>Phonebook</h1>
+        <h1 class="animate__animated animate__heartBeat">Phonebook</h1>
 
         <div className={styles.thumb}>
           <div>
             <img className={styles.image} src={Phone} alt="" />
           </div>
         </div>
-        <NavLink
-        to="/contacts"
-        className={styles.button}
-        title="Try it now!"
-      >
-        Try it now!
-      </NavLink>
+        <NavLink to="/contacts" className={styles.button} title="Try it now!">
+          Try it now!
+        </NavLink>
       </section>
     </main>
   );
